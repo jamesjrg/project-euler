a6924924d9fa62cfc6f7ff8efee6a8e8b2317a26
James Gregory <james.jrg@gmail.com>
2012-03-02 18:50:57 +0000

    25

--- euler/24.fs
+++ euler/24.fs
@@ -1,27 +1,17 @@
 ﻿module _24
 
-(*
-0 1 2 3 4 5 6 7 8 9 permutations
-permutations starting with a given number is (n - 1)! = 9! = 362880
-362880 * 2 is less than 1,000,000, whilst 362880 * 3 is greater than 1,000,000
-so permutation must start with 2
+open Lib
 
-permutations starting with 2, then another given number is (n -2)! = 8! = 40320
-so 2, 0 brings us up to 725760 + 40320 = 766080
-2, 1 up to 806400
-...2, 6 up to 967680
-2, 7 too many
-so begins with 2, 6
+let rec loop acc (digits_remain: int list) target_remain =
+    let curr_perms = int (Lib.factorial (bigint digits_remain.Length - 1I))
+    let num_increments = target_remain / curr_perms
+    let curr_digit = digits_remain.[num_increments]
+    let new_digits_remain = List.filter (fun x -> x <> curr_digit) digits_remain
+    if new_digits_remain = [] then curr_digit :: acc
+    else
+        loop (curr_digit :: acc) new_digits_remain (target_remain - curr_perms * num_increments)
 
-(n - 3)! = 7! = 5040
-2,6,0 up to 972720
-2,6,1 up to 977760
-2,6,2 up to 982800
-
-etc
-*)
-
-
+let answer = List.rev (loop [] [0..9] 999999)
 
 
 
---
+++ euler/25.fs
@@ -0,0 +1,5 @@
+﻿module _25
+
+let fib_gen = Seq.unfold (fun state -> Some(fst state + snd state, (snd state, fst state + snd state))) (1I,1I)
+let fib = Seq.append (seq {yield 1I; yield 1I}) fib_gen
+let answer = (Seq.findIndex (fun x -> (string x).Length = 1000) fib) + 1
