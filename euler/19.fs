03e2197296789c20c3fad2884735b4ded7a488a9
James Gregory <james.jrg@gmail.com>
2012-03-02 12:12:04 +0000

    19, 21

---
+++ euler/19.fs
@@ -0,0 +1,11 @@
+﻿module _19
+
+open System
+
+let mutable day = DateTime(1901, 1, 1)
+let mutable first_day_sunday_count = 0
+
+while day.Year <> 2001 do
+    day <- day.AddMonths(1)
+    if day.Day = 1 && day.DayOfWeek = DayOfWeek.Sunday then
+        first_day_sunday_count <- first_day_sunday_count + 1
---
+++ euler/21.fs
@@ -0,0 +1,17 @@
+﻿module _21
+
+let sum_divisors n =
+    Seq.collect (fun elem ->
+        if n % elem <> 0 then Seq.empty
+        else if elem = 1 || elem * elem = n then seq {yield elem}
+        else seq {yield elem; yield n / elem}) {1..int(sqrt (double n))} |> Seq.sum
+
+let amicable_sum n =
+    let da = sum_divisors n
+    let db = sum_divisors da
+    if db = n && n <> da then da else 0
+
+let answer = Seq.fold (fun acc elem -> acc + (amicable_sum elem)) 0 {1..9999}
+
+
+
