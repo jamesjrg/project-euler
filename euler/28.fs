1af8cbc9ce52e16b6261b6718b0c84821bbc2a95
James Gregory <james.jrg@gmail.com>
2012-03-04 20:55:48 +0000

    and an imperative 28, which seems cleaner than recursion

--- euler/28.fs
+++ euler/28.fs
@@ -1,8 +1,9 @@
 ﻿module _28
 
 let dim = 5
-let max_num = dim * dim
 
+//much better than first version, using some extremely basic maths
+let max_num = dim * dim
 let answer =
     let rec loop acc inc num_inc n =
         if n > max_num then (acc, n)
@@ -12,3 +13,14 @@ let answer =
                 else inc, num_inc + 1
             loop (acc + n) new_inc new_num_inc (n + inc)            
     loop 0 2 0 1
+
+//almost the same as above, but in imperative style. Actually seems much clearer this way...
+let mutable curr = 1
+let mutable total = 1
+let mutable add = 2
+for a in {1..(dim - 1) / 2} do
+    for b in {1..4} do
+        curr <- curr + add
+        total <- total + curr
+    add <- add + 2
+
