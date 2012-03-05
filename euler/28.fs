86e5763de39e4da29ae2ab6d0436937b6c2a6cb3
James Gregory <james.jrg@gmail.com>
2012-03-05 00:58:37 +0000

    superficial changes

--- euler/28.fs
+++ euler/28.fs
@@ -1,26 +1,28 @@
 ﻿module _28
 
-let dim = 5
+let dim = 1001
 
 //much better than first version, using some extremely basic maths
-let max_num = dim * dim
 let answer =
-    let rec loop acc inc num_inc n =
+    let max_num = dim * dim
+    let rec loop acc inc inc_count n =
         if n > max_num then (acc, n)
         else
-            let new_inc, new_num_inc =
-                if num_inc = 3 then inc + 2, 0
-                else inc, num_inc + 1
-            loop (acc + n) new_inc new_num_inc (n + inc)            
-    loop 0 2 0 1
+            let new_inc, new_inc_count =
+                if inc_count = 4 then inc + 2, 1
+                else inc, inc_count + 1
+            loop (acc + n) new_inc new_inc_count (n + inc)
+    loop 0 2 1 1
 
 //almost the same as above, but in imperative style. Actually seems much clearer this way...
-let mutable curr = 1
-let mutable total = 1
-let mutable add = 2
-for a in {1..(dim - 1) / 2} do
-    for b in {1..4} do
-        curr <- curr + add
-        total <- total + curr
-    add <- add + 2
+let answer_imperative = 
+    let mutable curr = 1
+    let mutable total = 0
+    let mutable add = 2
+
+    for a in {1..(dim - 1) / 2} do
+        for b in {1..4} do
+            curr <- curr + add
+            total <- total + curr
+        add <- add + 2
 
