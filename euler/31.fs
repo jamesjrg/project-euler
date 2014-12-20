3ebe1baab059d227f3ac6b8d0264fc03079b8856
James Gregory <james.jrg@gmail.com>
2014-12-20 16:46:15 +0000

    nice solution to 31 blatantly copied off the interwebs

--- euler/31.fs
+++ euler/31.fs
@@ -3,27 +3,16 @@
 open System
 open Lib
 
-//n choose r with repetitions allowed is equivalent to (n + r - 1) choose r
-//http://www.mathsisfun.com/combinatorics/combinations-permutations.html
-let ncr_with_rep n r =
-    Lib.ncr (n + r - 1I) r
+//blatantly copied off the interwebs
+//http://www.mathblog.dk/project-euler-31-combinations-english-currency-denominations/
 
-let rec combinations = Lib.memoize (fun x ->
-    match x with
-    | x when x = 200I -> 1I + ncr_with_rep (combinations 100I) 2I
-    //£1, 5*20p, or 2 50s made up variously
-    | x when x = 100I -> 2I + ncr_with_rep (combinations 50I) 2I
-    | x when x = 50I -> 1I +
-        //2*20p
-        (combinations 10I) +
-        //1*20p
-        ncr_with_rep (combinations 10I) 3I +
-        //0*20p
-        ncr_with_rep (combinations 10I) 5I
-    //1 10, 5*2p, or 2 5s made up variously
-    | x when x = 10I -> 2I + ncr_with_rep (combinations 5I) 2I
-    //1 5, 2 2s and a 1, 1 2 and 3 1s, 5 1s
-    | x when x = 5I -> 4I)
+let target = 200;
+let coinSizes = [| 1; 2; 5; 10; 20; 50; 100; 200 |]
+let ways = Array.create (target + 1) 0
+ways.[0] <- 1
 
-let answer = combinations 10I
+for i = 0 to coinSizes.Length - 1 do
+    for j = coinSizes.[i] to target do
+        ways.[j] <- ways.[j] + ways.[j - coinSizes.[i]];
 
+let answer = ways.[target]
